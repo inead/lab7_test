942b43b47129390fe115cf005aa5d86b26cdaa6a
inead <gmd0770551048@gmail.com>
2025-05-17 17:08:22 +0300

    Initial commit

---
+++ client/src/main/java/client/App.java
@@ -0,0 +1,30 @@
+package client;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import client.cli.Runner;
+import client.network.UDPClient;
+import client.utility.console.StandardConsole;
+
+import java.io.IOException;
+import java.net.InetAddress;
+
+
+public class App {
+  private static final int PORT = 23586;
+  public static final Logger logger = LogManager.getLogger("ClientLogger");
+
+  public static void main(String[] args) {
+    var console = new StandardConsole();
+    try {
+      var client = new UDPClient(InetAddress.getLocalHost(), PORT);
+      var cli = new Runner(client, console);
+
+      cli.interactiveMode();
+    } catch (IOException e) {
+      logger.info("Невозможно подключиться к серверу.", e);
+      console.println("Невозможно подключиться к серверу!");
+    }
+  }
+}
